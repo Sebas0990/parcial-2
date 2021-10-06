95ba26846f8083067e40e2309ca3d63236290e34
Sebas0990 <88167644+Sebas0990@users.noreply.github.com>
2021-10-05 19:10:56 -0500

    Delete OOP directory

--- OOP/juego.java
+++
@@ -1,7 +0,0 @@
-public class juego extends producto {
-    public static void main(String[] args) {
-        int pv_juego = (int) setprice(30, 110);
-        int pc_juego = (int) setprice(15, 60);
-    }
-
-}
--- OOP/limpieza.java
+++
@@ -1,7 +0,0 @@
-public class limpieza extends producto {
-    public static void main(String[] args) {
-        int precio_venta = (int) setprice(5, 20);
-        int precio_compra = (int) setprice(1, 10);
-    }
-
-}
